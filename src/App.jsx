65fdfa6bc5ffe68ed78ef2b05827ce581b1f1931
David Marshall <dlmarshall3@protonmail.com>
2023-09-15 22:35:37 -0500

    changed title

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 function App() {
   return (
     <div className="text-center">
-      <h1 className="text-4xl mt-8">DS4A Cohort 4 TAs</h1>
+      <h1 className="text-4xl mt-8">Amazon Software Development Cohort 4 TAs</h1>
       <div className="lg:columns-3 column-1 my-8 mx-24 p-8 bg-white opacity rounded-2xl shadow-2xl">
         {cohortFourTAs.map((ta, index) => {
           return (
